fa322ecd280ce4ce1d9d8d469f548746a72e1e29
Georges <georges.alkhouri@gmail.com>
2026-09-25 20:31:02 +0200

    Add SEO foundations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://sealbreak.app",
   output: "static",
+  integrations: [sitemap()],
 });
